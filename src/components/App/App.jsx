47fafe8557f35c032b87abf24dbcdd540c926b36
MariDei <d.marianna@icloud.com>
2025-02-20 23:46:21 +0200

    Add files to folders "pages" & "components"

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,31 @@
-import './App.module.css';
+import { lazy, Suspense } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Loader from '../Loader/Loader';
+
+const HomePage = lazy(() => import('../../pages/HomePage/HomePage'));
+const TeachersPage = lazy(() =>
+  import('../../pages/TeachersPage/TeachersPage')
+);
+const FavoritesPage = lazy(() =>
+  import('../../pages/FavoritesPage/FavoritesPage')
+);
+const NotFoundPage = lazy(() =>
+  import('../../pages/NotFoundPage/NotFoundPage')
+);
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/teachers" element={<TeachersPage />} />
+          <Route path="/favorites" element={<FavoritesPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Suspense>
+    </>
+  );
 }
 
 export default App;
