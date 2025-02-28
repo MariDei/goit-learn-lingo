4f94a1dd043a6c0f5daa5e052e8918adea9427c6
MariDei <d.marianna@icloud.com>
2025-02-28 23:58:11 +0200

    Install packages

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import { store } from './redux/store.js';
+// import { store } from './redux/store.js';
 import App from './components/App/App';
 import 'modern-normalize';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
+    <Provider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
