26544819eeb3ef09f6a3b4dbecc54b7b761b7c9d
MariDei <d.marianna@icloud.com>
2025-03-04 23:51:11 +0200

    Update component AuthMenu.jsx

--- src/components/AuthMenu/AuthMenu.jsx
+++ src/components/AuthMenu/AuthMenu.jsx
@@ -1,16 +1,24 @@
 import icons from '../../images/icons.svg';
 import css from './AuthMenu.module.css';
 
-const AuthMenu = () => {
+const AuthMenu = ({ openModal }) => {
   return (
     <div className={css.wrapperBtns}>
-      <button className={css.loginBtn} type="button">
+      <button
+        className={css.loginBtn}
+        type="button"
+        onClick={() => openModal('login')}
+      >
         <svg width="20" height="20">
           <use href={`${icons}#${'icon-log-in'}`}></use>
         </svg>
         Log in
       </button>
-      <button className={css.regBtn} type="button">
+      <button
+        className={css.regBtn}
+        type="button"
+        onClick={() => openModal('registration')}
+      >
         Registration
       </button>
     </div>
